32ca11cf3c8b623157e2713aead3e649c05f4d56
Valentin447 <valentin@myttk.ru>
2022-09-28 20:55:24 +0700

    Add solution task 27

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
 ﻿void Task25()
 {
+    // Задача 25: Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+    Console.Write("Введите число А: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int res = a;
 
+    for(int i = 2; i <= b; i++)
+    {
+        res = res * a;
+    }
+    Console.WriteLine($"{a} в степени {b} равно {res}");
 }
 void Task27()
 {
+    // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    Console.Write("Введите число: ");
+    int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    int sum = 0;
 
+    while(number > 0){
+        sum = sum + (number % 10);
+        number /= 10;
+    }
+    Console.WriteLine($"Сумма цифр равна {sum}");
 }
 void Task29()
 {
 
 }
 
-Task25();
-//Task27();
+//Task25();
+Task27();
 //Task29();
